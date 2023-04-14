1aaba02d02ad3514dae1c93499ab907558cbb544
Stefan de Konink <stefan@konink.de>
2023-04-14 16:52:14 +0200

    The configuration.
    For tokens.
    
    NOVA: contact SBB
    OJP: https://opentransportdata.swiss/de/dev-dashboard/

---
+++ configuration.py
@@ -0,0 +1,7 @@
+# CONFIGURATION:
+NOVA_URL_TOKEN = "https://login.microsoftonline.com/2cda5d11-f0ac-46b3-967d-af1b2e1bd01a/oauth2/v2.0/token"
+NOVA_CLIENT_ID = ''
+NOVA_CLIENT_SECRET = ''
+NOVA_URL_API = "https://nova-inte-int.api.sbb.ch/novaan/vertrieb/public/v14/PreisauskunftService"
+OJP_URL_API = "https://api.opentransportdata.swiss/ojp2020"
+OJP_TOKEN = ''
