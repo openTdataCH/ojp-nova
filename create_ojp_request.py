2844a7e48aa7f458dfce9fee871bdece7855cec9
Matthias Günter <matthias.guenter@sbb.ch>
2023-04-21 17:20:33 +0200

    second part

--- create_ojp_request.py
+++ create_ojp_request.py
@@ -7,24 +7,37 @@ from xsdata.models.datatype import XmlDateTime
 from ojp import Ojp, Ojprequest, ServiceRequest, OjptripRequest, PlaceContextStructure, TripParamStructure, \
     PlaceRefStructure, InternationalTextStructure, NaturalLanguageStringStructure
 
-def test_create_ojp_trip_request() -> Ojp:
-    now = datetime.datetime.utcnow().isoformat() + "Z"
-    now = XmlDateTime.from_string(now)
+def test_create_ojp_trip_request_simple_1() -> Ojp:
+    origin='8507000'
+    destination='8503000'
+    starttime=datetime.datetime.utcnow()+ datetime.timedelta(days=10) #10 days in the future
+    return test_create_ojp_trip_request_long(origin,destination,starttime)
+
+def test_create_ojp_trip_request_simple_2() -> Ojp:
+    origin='857868'
+    destination='8574945'
+    starttime=datetime.datetime.utcnow()+ datetime.timedelta(days=10) #10 days in the future
+    return test_create_ojp_trip_request_long(origin,destination,starttime)
+
+
+def test_create_ojp_trip_request_long(origin,destination,starttime) -> Ojp:
+    starttime = starttime.isoformat() + "Z"
+    starttime = XmlDateTime.from_string(starttime)
 
     return Ojp(ojprequest=
                Ojprequest(service_request=
                    ServiceRequest(requestor_ref="OJP2NOVA",
-                                  request_timestamp=now,
+                                  request_timestamp=starttime,
                                   ojptrip_request=
                                   [OjptripRequest(
-                                      request_timestamp=now,
+                                      request_timestamp=starttime,
                                       origin=[PlaceContextStructure(place_ref=
-                                                                    PlaceRefStructure(stop_point_ref=str(8507000),
-                                                                                      location_name=InternationalTextStructure(text=NaturalLanguageStringStructure("Bern"))),
-                                                                    dep_arr_time=now)],
+                                                                    PlaceRefStructure(stop_point_ref=str(origin),
+                                                                                      location_name=InternationalTextStructure(text=NaturalLanguageStringStructure("origin"))),
+                                                                    dep_arr_time=starttime)],
                                       destination=[PlaceContextStructure(place_ref=
-                                                                         PlaceRefStructure(stop_point_ref=str(8503000),
-                                                                                           location_name=InternationalTextStructure(text=NaturalLanguageStringStructure("Zurich"))))
+                                                                         PlaceRefStructure(stop_point_ref=str(destination),
+                                                                                           location_name=InternationalTextStructure(text=NaturalLanguageStringStructure("destination"))))
                                                                          ],
                                       params=TripParamStructure(number_of_results=5,
                                                                 include_track_sections=False,
--- network_flow.py
+++ network_flow.py
@@ -10,7 +10,7 @@ from xsdata.formats.dataclass.serializers import XmlSerializer
 from xsdata.formats.dataclass.serializers.config import SerializerConfig
 
 from configuration import *
-from create_ojp_request import test_create_ojp_trip_request
+from create_ojp_request import test_create_ojp_trip_request_simple_2
 from map_nova_to_ojp import test_nova_to_ojp
 from map_ojp_to_nova import test_ojp_fare_request_to_nova_request
 from map_ojp_to_ojp import parse_ojp, map_ojp_trip_result_to_ojp_fare_request, \
@@ -85,7 +85,7 @@ def test_nova_request_reply(ojp: Ojp):
         return nova_response
 
 if __name__ == '__main__':
-    ojp_trip_request = test_create_ojp_trip_request()
+    ojp_trip_request = test_create_ojp_trip_request_simple_2()
     serializer_config = SerializerConfig(ignore_default_attributes=True, pretty_print=True)
     serializer = XmlSerializer(serializer_config)
     ojp_trip_request_xml = serializer.render(ojp_trip_request, ns_map=ns_map)
