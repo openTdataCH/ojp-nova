803cddcfe6ac6805d0253eae6fca84d556294e42
Stefan de Konink <stefan@konink.de>
2023-05-10 17:16:19 +0200

    Example

--- logger.py
+++ logger.py
@@ -3,6 +3,7 @@ from configuration import *
 import logging
 from logging.handlers import RotatingFileHandler
 import datetime
+import sys
 
 # writing debugging files and specific points
 def log(filename,xml):
@@ -20,3 +21,25 @@ def log_entry(origin, destination, starttime,stoptime):
     t = datetime.datetime.now(tz=tz).strftime(ft)
     logstr='"'+t+'";"'+origin+'";"'+destination+"';"+starttime+"';"+stoptime
     logger.info(logstr)
+
+def setup_custom_logger():
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+
+    handler = RotatingFileHandler(LOGFILE, maxBytes=80000, backupCount=10)
+    handler.setFormatter(formatter)
+
+    screen_handler = logging.StreamHandler(stream=sys.stdout)
+    screen_handler.setFormatter(formatter)
+
+    logger = logging.getLogger("OJP2NOVA")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.addHandler(screen_handler)
+
+    return logger
+
+import pathlib
+pathlib.Path("generated").mkdir(parents=True, exist_ok=True)
+logger = setup_custom_logger()
+
