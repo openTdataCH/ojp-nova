e901ad41dc68d8d4977a68d7b749d0407c779a37
Stefan de Konink <stefan@konink.de>
2023-05-10 11:27:43 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,13 +1,16 @@
 # CONFIGURATION:
 
-# For keys and connection to NOVA contact opendata@sbb.ch if necessary. However, those access keys only are provided under special
-# circumstances. OJP_Token can be obtained at: https://opentransportdata.swiss/dev-dashboard
+# For keys and connection to NOVA contact opendata@sbb.ch if necessary.
+# However, those access keys only are provided under special circumstances.
 NOVA_URL_TOKEN = ""
 NOVA_CLIENT_ID = ''
 NOVA_CLIENT_SECRET = ''
 NOVA_URL_API = ""
+
+# OJP_Token can be obtained at: https://opentransportdata.swiss/dev-dashboard
 OJP_URL_API = "https://api.opentransportdata.swiss/ojp2020"
 OJP_TOKEN = ""
+
 HTTPS = False
 SSL_KEYFILE = ''
 SSL_CERTFILE = ''
@@ -16,4 +19,3 @@ HTTP_PORT = 8000
 HTTP_SLUG = "ojp2023"
 DEBUGGING = True
 LOGFILE = "generated/my_log.log"
-
