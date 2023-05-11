6a7aa8e01815f80c5be3eba8ee91830d83c554f5
Stefan de Konink <stefan@konink.de>
2023-05-11 15:25:06 +0200

    Inband OJP-errors

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
+import datetime
 
 from fastapi import FastAPI, Request, Response, HTTPException
 from xsdata.formats.dataclass.serializers import XmlSerializer
 from xsdata.formats.dataclass.serializers.config import SerializerConfig
+from xsdata.models.datatype import XmlDateTime
 
 from map_nova_to_ojp import test_nova_to_ojp
-from map_ojp_to_ojp import map_ojp_trip_result_to_ojp_fare_request, parse_ojp
+from map_ojp_to_ojp import parse_ojp
+from ojp import Ojp, Ojpresponse, ServiceDelivery, ServiceDeliveryStructure, \
+    OtherError
 from test_network_flow import test_nova_request_reply, call_ojp_2000
 
 from configuration import HTTP_HOST, HTTP_PORT, HTTPS, SSL_CERTFILE, SSL_KEYFILE, HTTP_SLUG
@@ -16,6 +20,13 @@ serializer_config = SerializerConfig(ignore_default_attributes=True, pretty_prin
 serializer = XmlSerializer(serializer_config)
 
 ns_map = {'': 'http://www.siri.org.uk/siri', 'ojp': 'http://www.vdv.de/ojp'}
+
+def error_response(error_text:str):
+    return Ojp(ojpresponse=
+        Ojpresponse(service_delivery=
+                    ServiceDelivery(response_timestamp=XmlDateTime.from_datetime(datetime.datetime.utcnow()),
+                                    producer_ref="OJP2NOVA",
+                                    error_condition=ServiceDeliveryStructure.ErrorCondition(other_error=OtherError(error_text)))))
 
 @app.post("/"+HTTP_SLUG, tags=["Open Journey Planner"])
 async def post_request(fastapi_req: Request):
@@ -30,15 +41,14 @@ async def post_request(fastapi_req: Request):
                 ojp_fare_result_xml = serializer.render(ojp_fare_result, ns_map=ns_map)
                 return Response(ojp_fare_result_xml, media_type="application/xml; charset=utf-8")
 
-            # TODO: In band error error reply?
-            raise HTTPException(status_code=400, detail="There was no NOVA response")
+            return Response(serializer.render(error_response("There was no NOVA response"), ns_map=ns_map), status_code=400, media_type="application/xml; charset=utf-8")
 
         else:
             return Response(call_ojp_2000(body.decode('utf-8')), media_type="application/xml; charset=utf-8")
 
     else:
-        # TODO: In band error error reply?
-        raise HTTPException(status_code=400, detail="There was no OJP request")
+        return Response(serializer.render(error_response("There was no (valid) OJP request"), ns_map=ns_map), status_code=400,
+                        media_type="application/xml; charset=utf-8")
 
 if __name__ == "__main__":
     import uvicorn
