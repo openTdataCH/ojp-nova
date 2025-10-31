f56a316b1c58efd6a07f4fd9e8e942fcc21d4288
u239816 <urs.stoeckli@sbb.ch>
2025-10-31 15:55:34 +0100

    SKIPLUS-1012: fix configuration issues.

--- test_network_flow.py
+++ test_network_flow.py
@@ -10,7 +10,7 @@ from xsdata.formats.dataclass.serializers import XmlSerializer
 from xsdata.formats.dataclass.serializers.config import SerializerConfig
 
 import xml_logger
-from configuration import NOVA_BASE_URL, OJP_URL_API,OJP_TOKEN, NOVA_URL_TOKEN, NOVA_CLIENT_ID, NOVA_CLIENT_SECRET, READFILE, READTRIPREQUESTFILE
+from configuration import NOVA_URL_API, OJP_URL_API,OJP_TOKEN, NOVA_URL_TOKEN, NOVA_CLIENT_ID, NOVA_CLIENT_SECRET, READFILE, READTRIPREQUESTFILE
 from test_create_ojp_request import *
 from map_nova_to_ojp import test_nova_to_ojp
 from map_ojp_to_nova import test_ojp_fare_request_to_nova_request
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 
 ns_map = {'': 'http://www.siri.org.uk/siri', 'ojp': 'http://www.vdv.de/ojp'}
 
-NOVA_URL_API = NOVA_BASE_URL + "/novaan/vertrieb/public/v14/PreisauskunftService"
+NOVA_URL_API = NOVA_URL_API + "/novaan/vertrieb/public/v14/PreisauskunftService"
 
 def call_ojp_2000(request_body):
     try:
